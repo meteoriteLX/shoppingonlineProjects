a251f1e99dc87bdf7e470fe4aa6181697172f6d3
meteoriteLX <15021762+meteoritelx@user.noreply.gitee.com>
2024-12-11 17:07:34 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+//<script>
+//export default{
+ // setup() {
+ //   const message = 'this is message'
+ //   const logMessage = () => {
+  //    console.log(message)
+  //  }
+   // return {
+  //    message,
+   //   logMessage
+  //  }
+ // }
+//}
+//</script>
+
+//以上的简单写法：
+<script setup>
+const message = 'this is message'
+const logMessage = () => {
+  console.log(message)
+}
+</script setup>
+
+<template>  
+  <div>
+    {{ message }}
+    <button @click="logMessage"> log </button>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
